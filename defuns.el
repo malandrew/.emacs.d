ea0395e1dbeb9cc9ed839d3de683be2b74a5b768
Magnar Sveen <magnars@gmail.com>
2011-11-06 10:29:35 +0100

    Cleanup

--- defuns.el
+++ defuns.el
@@ -250,8 +250,7 @@ Both PATTERN and CONTENTS are matched as regular expressions."
 (defun current-quotes-char ()
   (nth 3 (syntax-ppss)))
 
-(defun point-is-in-string-p ()
-  (current-quotes-char))
+(defalias 'point-is-in-string-p 'current-quotes-char)
 
 (defun move-point-forward-out-of-string ()
   (while (point-is-in-string-p) (forward-char)))
