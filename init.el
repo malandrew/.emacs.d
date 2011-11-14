2acce5d73505df80b112a1daca2a5510f3e2de2b
Christian Johansen <christian.johansen@finn.no>
2011-11-14 07:16:55 +0100

    Ask before closing

--- init.el
+++ init.el
@@ -201,3 +201,15 @@
 (require 'server)
 (unless (server-running-p)
   (server-start))
+
+(defun ask-before-closing ()
+  "Ask whether or not to close, and then close if y was pressed"
+  (interactive)
+  (if (y-or-n-p (format "Are you sure you want to exit Emacs? "))
+      (if (< emacs-major-version 22)
+          (save-buffers-kill-terminal)
+        (save-buffers-kill-emacs))
+    (message "Canceled exit")))
+
+(when window-system
+  (global-set-key (kbd "C-x C-c") 'ask-before-closing))
