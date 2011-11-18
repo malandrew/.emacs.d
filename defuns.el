f218d96e24ff1506ec8552eb3bfad9ecbcf4bf5f
Christian Johansen <christian.johansen@finn.no>
2011-11-18 07:22:38 +0100

    Fix docstring

--- defuns.el
+++ defuns.el
@@ -168,9 +168,8 @@ If there's no region, the current line will be duplicated."
 
 (defun duplicate-region (num &optional start end)
   "Duplicates the region bounded by START and END NUM times.
-If no START and END is provided, the current region-beginning
-and region-end is used. Moves the active region to the text
-last inserted."
+If no START and END is provided, the current region-beginning and
+region-end is used. Adds the duplicated text to the kill ring."
   (interactive "p")
   (let* ((start (or start (region-beginning)))
          (end (or end (region-end)))
